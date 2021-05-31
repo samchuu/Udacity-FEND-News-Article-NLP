33bdb2c6db3a8a5497793917bb4cb20652d9c29e
Sam Chu <samchupiano@gmail.com>
2021-06-01 01:28:09 +0200

    add test with jest

---
+++ __test__/testCheckURL.spec.js
@@ -0,0 +1,8 @@
+import { validateURL } from "../src/client/js/urlChecker"
+
+
+describe("Testing the submit functionality", () => {
+  
+    test("Testing the checkForURL() function", () => {
+           expect(validateURL).toBeDefined();
+})});
